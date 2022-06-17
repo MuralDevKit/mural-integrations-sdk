0023b9c793c623e6a3e9728e2bb4484f20cc8186
Micael Bergeron <mbergeron@mural.co>
2022-06-17 14:19:24 -0400

    Update the documentation around the sample

--- packages/mural-canvas/src/index.ts
+++ packages/mural-canvas/src/index.ts
@@ -1,6 +1,8 @@
-import Canvas from './components/canvas';
-import SessionActivation from './components/session-activation';
-import RpcClient from './rpc';
 import '@muraldevkit/mural-integrations-common/styles/fonts.css';
 
+import Canvas from './components/canvas';
+import SessionActivation from './components/session-activation';
+import RpcClient from './lib/rpc';
+
+export * from './lib/session-activation';
 export { Canvas, SessionActivation, RpcClient };
