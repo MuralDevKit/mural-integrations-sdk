e03b4baa960eb67c4282b1917f77215238953824
Max Smolens <msmolens@mural.co>
2023-03-31 16:33:55 -0400

    Add test steps for fake timers
    Add the test steps of the following format to enable controlling the
    flow of time in tests:
    
    - Given a fake timer
    - When the fake timer advances 500 ms

--- test/react/step-definitions/given/index.ts
+++ test/react/step-definitions/given/index.ts
@@ -3,9 +3,11 @@ import { SetupFnArgs } from 'pickled-cucumber/types';
 import api from './api';
 import misc from './misc';
 import route from './route';
+import timer from './timer';
 
 export default function registerGiven(args: SetupFnArgs) {
   api(args);
   misc(args);
   route(args);
+  timer(args);
 }
--- test/react/step-definitions/when/index.ts
+++ test/react/step-definitions/when/index.ts
@@ -2,10 +2,12 @@ import { SetupFnArgs } from 'pickled-cucumber/types';
 
 import input from './input';
 import misc from './misc';
+import timer from './timer';
 import window from './window';
 
 export default function registerWhen(args: SetupFnArgs) {
   input(args);
   misc(args);
+  timer(args);
   window(args);
 }
